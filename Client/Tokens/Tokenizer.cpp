de637ba94cf5f71ba25274834f5897535a13e022
Carlos Akion Garro Campos <carloscamp1008@estudiantec.cr>
2021-04-26 15:02:19 -0600

    IDP1-32 Update IDE

--- Client/Tokens/Tokenizer.cpp
+++ Client/Tokens/Tokenizer.cpp
@@ -12,125 +12,152 @@ namespace interpreter{
         currentToken.mLineNumber = 1;
 
         for (char currCh : inProgram) {
-            switch (currCh) {
-                case '0':
-                case '1':
-                case '2':
-                case '3':
-                case '4':
-                case '5':
-                case '6':
-                case '7':
-                case '8':
-                case '9':
-                    if (currentToken.mType == WHITESPACE) {
-                        currentToken.mType = INTEGER_LITERAL;
-                        currentToken.mText.append(1, currCh);
-                    } else if (currentToken.mType == POTENTIAL_DOUBLE) {
-                        currentToken.mType = DOUBLE_LITERAL;
-                        currentToken.mText.append(1, currCh);
-                    } else {
-                        currentToken.mText.append(1, currCh);
-                    }
-                    break;
-
-                case '.':
-                    if (currentToken.mType == WHITESPACE) {
-                        currentToken.mType = POTENTIAL_DOUBLE;
-                        currentToken.mText.append(1, currCh);
-                    } else if (currentToken.mType == INTEGER_LITERAL) {
-                        currentToken.mType = DOUBLE_LITERAL;
-                        currentToken.mText.append(1, currCh);
-                    } else if (currentToken.mType == STRING_LITERAL) {
-                        currentToken.mText.append(1, currCh);
-                    } else {
-                        endToken(currentToken, tokens);
-                        currentToken.mType = OPERATOR;
-                        currentToken.mText.append(1, currCh);
-                        endToken(currentToken, tokens);
-                    }
-                    break;
-
-                case '{':
-                case '}':
-                case '(':
-                case ')':
-                case '=':
-                case '+':
-                case '-':
-                case '*':
-                case ';':
-                case ',':
-                    if (currentToken.mType != STRING_LITERAL) {
-                        endToken(currentToken, tokens);
-                        currentToken.mType = OPERATOR;
-                        currentToken.mText.append(1, currCh);
-                        endToken(currentToken, tokens);
-                    } else {
-                        currentToken.mText.append(1, currCh);
-                    }
-                    break;
-
-                case ' ':
-                case '\t':
-                    if (currentToken.mType == STRING_LITERAL || currentToken.mType == COMMENT) {
-                        currentToken.mText.append(1, currCh);
-                    } else {
-                        endToken(currentToken, tokens);
-                    }
-                    break;
-
-
-                case '\n':
-                    endToken(currentToken, tokens);
-                    ++currentToken.mLineNumber;
-                    break;
-
-                case '"':
-                    if (currentToken.mType != STRING_LITERAL) {
-                        endToken(currentToken, tokens);
+                    if (currentToken.mType == STRING_ESCAPE_SEQUENCE) {
+                        switch (currCh) {
+                            case 'n':
+                                currentToken.mText.append(1, '\n');
+                                break;
+                            case 'r':
+                                currentToken.mText.append(1, '\r');
+                                break;
+                            case 't':
+                                currentToken.mText.append(1, '\t');
+                                break;
+                            case '\\':
+                                currentToken.mText.append(1, '\\');
+                                break;
+                            default:
+                                throw runtime_error(string("unknown escape sequence: \\") + string(1, currCh) +
+                                                    " in string on line " + to_string(currentToken.mLineNumber) + ".");
+                                break;
+                        }
                         currentToken.mType = STRING_LITERAL;
-                    } else if (currentToken.mType == STRING_LITERAL) {
-                        endToken(currentToken, tokens);
-                    }
-                    break;
-
-                case '\\':
-                    if (currentToken.mType == STRING_LITERAL) {
-                        currentToken.mType = STRING_ESCAPE_SEQUENCE;
-                    } else {
-                        endToken(currentToken, tokens);
+                        continue;
+                    } else if (currentToken.mType == POTENTIAL_COMMENT && currCh != '/') {
                         currentToken.mType = OPERATOR;
-                        currentToken.mText.append(1, currCh);
                         endToken(currentToken, tokens);
+                        continue;
                     }
-                    break;
 
-                case '/':
-                    if (currentToken.mType == STRING_LITERAL) {
-                        currentToken.mText.append(1, currCh);
-                    } else if (currentToken.mType == POTENTIAL_COMMENT) {
-                        currentToken.mType = COMMENT;
-                        currentToken.mText.erase();
-                    } else {
-                        endToken(currentToken, tokens);
-                        currentToken.mType = POTENTIAL_COMMENT;
-                        currentToken.mText.append(1, currCh);
-                    }
-                    break;
+                    switch (currCh) {
+                        case '0':
+                        case '1':
+                        case '2':
+                        case '3':
+                        case '4':
+                        case '5':
+                        case '6':
+                        case '7':
+                        case '8':
+                        case '9':
+                            if (currentToken.mType == WHITESPACE) {
+                                currentToken.mType = INTEGER_LITERAL;
+                                currentToken.mText.append(1, currCh);
+                            } else if (currentToken.mType == POTENTIAL_DOUBLE) {
+                                currentToken.mType = DOUBLE_LITERAL;
+                                currentToken.mText.append(1, currCh);
+                            } else {
+                                currentToken.mText.append(1, currCh);
+                            }
+                            break;
 
-                default:
-                    if (currentToken.mType == WHITESPACE || currentToken.mType == INTEGER_LITERAL
-                        || currentToken.mType == DOUBLE_LITERAL) {
-                        endToken(currentToken, tokens);
-                        currentToken.mType = IDENTIFIER;
-                        currentToken.mText.append(1, currCh);
-                    } else {
-                        currentToken.mText.append(1, currCh);
+                        case '.':
+                            if (currentToken.mType == WHITESPACE) {
+                                currentToken.mType = POTENTIAL_DOUBLE;
+                                currentToken.mText.append(1, currCh);
+                            } else if (currentToken.mType == INTEGER_LITERAL) {
+                                currentToken.mType = DOUBLE_LITERAL;
+                                currentToken.mText.append(1, currCh);
+                            } else if (currentToken.mType == STRING_LITERAL) {
+                                currentToken.mText.append(1, currCh);
+                            } else {
+                                endToken(currentToken, tokens);
+                                currentToken.mType = OPERATOR;
+                                currentToken.mText.append(1, currCh);
+                                endToken(currentToken, tokens);
+                            }
+                            break;
+
+                        case '{':
+                        case '}':
+                        case '(':
+                        case ')':
+                        case '=':
+                        case '+':
+                        case '-':
+                        case '*':
+                        case ';':
+                        case ',':
+                            if (currentToken.mType != STRING_LITERAL) {
+                                endToken(currentToken, tokens);
+                                currentToken.mType = OPERATOR;
+                                currentToken.mText.append(1, currCh);
+                                endToken(currentToken, tokens);
+                            } else {
+                                currentToken.mText.append(1, currCh);
+                            }
+                            break;
+
+                        case ' ':
+                        case '\t':
+                            if (currentToken.mType == STRING_LITERAL || currentToken.mType == COMMENT) {
+                                currentToken.mText.append(1, currCh);
+                            } else {
+                                endToken(currentToken, tokens);
+                            }
+                            break;
+
+                        case '\r':
+                        case '\n':
+                            endToken(currentToken, tokens);
+                            ++currentToken.mLineNumber;
+                            break;
+
+                        case '"':
+                            if (currentToken.mType != STRING_LITERAL) {
+                                endToken(currentToken, tokens);
+                                currentToken.mType = STRING_LITERAL;
+                            } else if (currentToken.mType == STRING_LITERAL) {
+                                endToken(currentToken, tokens);
+                            }
+                            break;
+
+                        case '\\':
+                            if (currentToken.mType == STRING_LITERAL) {
+                                currentToken.mType = STRING_ESCAPE_SEQUENCE;
+                            } else {
+                                endToken(currentToken, tokens);
+                                currentToken.mType = OPERATOR;
+                                currentToken.mText.append(1, currCh);
+                                endToken(currentToken, tokens);
+                            }
+                            break;
+
+                        case '/':
+                            if (currentToken.mType == STRING_LITERAL) {
+                                currentToken.mText.append(1, currCh);
+                            } else if (currentToken.mType == POTENTIAL_COMMENT) {
+                                currentToken.mType = COMMENT;
+                                currentToken.mText.erase();
+                            } else {
+                                endToken(currentToken, tokens);
+                                currentToken.mType = POTENTIAL_COMMENT;
+                                currentToken.mText.append(1, currCh);
+                            }
+                            break;
+
+                        default:
+                            if (currentToken.mType == WHITESPACE || currentToken.mType == INTEGER_LITERAL
+                                                                    || currentToken.mType == DOUBLE_LITERAL) {
+                                endToken(currentToken, tokens);
+                                currentToken.mType = IDENTIFIER;
+                                currentToken.mText.append(1, currCh);
+                            } else {
+                                currentToken.mText.append(1, currCh);
+                            }
+                            break;
                     }
-                    break;
-            }
-        }
+                }
 
         endToken(currentToken, tokens);
 
